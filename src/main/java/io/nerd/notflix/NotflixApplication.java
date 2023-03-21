211c49d5200fa32c46489364b577a4936c309f94
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-03-21 12:43:45 +0200

    Initial commit

---
+++ src/main/java/io/nerd/notflix/NotflixApplication.java
@@ -0,0 +1,13 @@
+package io.nerd.notflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nerd/notflix/NotflixApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nerd.notflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
