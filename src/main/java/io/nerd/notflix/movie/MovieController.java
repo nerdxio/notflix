41347df27e3610593c47217875224e1bcdb213f6
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-03-22 11:49:55 +0200

    added Layout and Home component

--- src/main/java/io/nerd/notflix/movie/MovieController.java
+++ src/main/java/io/nerd/notflix/movie/MovieController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +16,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/movies")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:3000")
 public class MovieController {
     private final MovieService movieService;
 
--- src/main/java/io/nerd/notflix/review/ReviewController.java
+++ src/main/java/io/nerd/notflix/review/ReviewController.java
@@ -7,16 +7,14 @@ package io.nerd.notflix.review;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:3000")
 public class ReviewController {
 
     private final ReviewService reviewService;
